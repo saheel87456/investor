93681c51961ab1df7975b42800aa56446ceb1e65
saheel87456 <151958451+saheel87456@users.noreply.github.com>
2023-11-24 23:37:47 +0530

    Update bankaccocunt.sol

--- bankaccocunt.sol
+++ bankaccocunt.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Bank_Account {
+    address public investor;
+    uint256 public capital;
+
+    constructor() {
+        investor = msg.sender;
+    }
+
+    function investCapital(uint256 funds) public {
+        require(msg.sender == investor, "Only the investor can invest");
+
+        // Ensure a positive investment amount
+        assert(funds > 0);
+
+        capital += funds;
+    }
+
+    function withdrawCapital(uint256 funds) public {
+        if (msg.sender != investor) {
+            revert("Only the investor can withdraw the capital");
+        }
+
+        // Ensure a non-negative remaining capital
+        require(funds <= capital, "Withdrawal amount exceeds capital");
+
+        capital -= funds;
+    }
+
+    function getCapital() public view returns (uint256) {
+        return capital;
+    }
+}
